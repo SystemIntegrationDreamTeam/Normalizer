85cb60a5e83e4590254501d6190f684601f2ea04
bastianbuhrkall <bastian_buhrkall@hotmail.com>
2016-12-30 23:32:26 +0100

    Git Commit

---
+++ src/main/java/com/mycompany/normalizer/JResponse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.normalizer;
+
+/**
+ *
+ * @author Buhrkall
+ */
+public class JResponse {
+
+    String ssn;
+    String interestRate;
+
+    public JResponse(String ssn, String interestRate) {
+        this.ssn = ssn;
+        this.interestRate = interestRate;
+    }
+
+}
